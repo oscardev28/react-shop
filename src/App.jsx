020936752c262550a144781ad1f7abaf4c391652
Óscar Martín-Hervás Ruiz <oscardevelop28@gmail.com>
2025-07-04 18:48:20 +0200

    Primera fase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 import { products as initialProducts } from './mocks/products.json'
 
 function App() {
+  const [filter, setFilters] = useState({
+    minPrice: 0,
+    category: 'all'
+  })
+  const filterProducts = products.filter(item => {
+    if(
+      item.price >= filter.minPrice && (
+        item.category === filter.category || filter.category === 'all'
+      )
+    ) {
+      return item
+    }
+  })
 
   return (
     <>
